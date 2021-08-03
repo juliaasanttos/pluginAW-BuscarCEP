cabc9c3c9c0d35f2ad43c5f3cff24470b9bdf311
Júlia Santos <julia.asanttos04@gmail.com>
2021-08-03 18:52:40 -0300

    inicio da criação de plugins (buscar cep)

---
+++ busca-cep.php
@@ -0,0 +1,12 @@
+/**
+* Plugin Name: Buscar CEP
+* Description: Plugin que busca dados do endereço de uma pessoa pelo CEP
+* Version: 1.0
+* Author: Júlia Araújo Santos Jorge
+*/
+
+function busca_cep(){
+    return "<h3>Teste cep </h3>"
+}
+
+add_shortcode('buscaCep', 'busca_cep');
